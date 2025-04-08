04910395f6bdd23d9b17c62e01be19c22f5e94d3
Kirti7310 <karapurkarkirti@gmail.com>
2025-04-08 12:55:37 +0530

    INITIAL COMMIT

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,25 @@
+dmx.config({
+  "booking_form": {
+    "data_detail1": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "event_name"
+        },
+        {
+          "type": "number",
+          "name": "booking_id"
+        },
+        {
+          "type": "date",
+          "name": "booking_date"
+        },
+        {
+          "type": "text",
+          "name": "status"
+        }
+      ],
+      "outputType": "array"
+    }
+  }
+});
